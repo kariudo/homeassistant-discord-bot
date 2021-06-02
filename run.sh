dfa9ffff4e9e0e571c31af009454b0cc1968d174
kjell <kjell5317@gmail.com>
2021-06-02 13:52:26 +0200

    fix

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/with-contenv bashio
+set +u
+
+CONFIG_PATH=/data/options.json
+
+BOT_TOKEN=$(baashio::config 'bot_token')
+URL=$(bashio::config 'mqtt_url')
+PORT=$(bashio::config 'mqtt_port')
+USERNAME=$(bashio::config 'mqtt_username')
+PASSWORD=$(bashio::config 'mqtt_password')
+ONLINE=$(bashio::config 'topic_online_friends')
+VOICE=$(bashio::config 'topic_voice_connected')
+COMMAND=$(bashio::config 'topic_command')
+GUILD=$(bashio::config 'guild_id')
+YOUR=$(bashio::config 'your_id')
+
+bashio::log.info "Starting..."
+
+node ./server.js $BOT_TOKEN $URL $PORT $USERNAME $PASSWORD $ONLINE $COMMAND $VOICE $GUILD $YOUR
