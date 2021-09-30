3baef5b34f1350ced6d0e0bb5c063d934c89c555
kjell <kjell5317@gmail.com>
2021-09-30 23:39:39 +0200

    simplify

--- server.js
+++ server.js
@@ -83,21 +83,12 @@ d_client.on("voiceStateUpdate", (oldState, newState) => {
         '{"voice_connection":"false", "mute": "unavailable", "deaf": "unavailable"}'
       );
     }
-  } else if (oldState.channelID === null) {
-    if (newState.member.id === your_id) {
-      deaf = newState.member.voice.deaf;
-      mute = newState.member.voice.mute;
-      voice = { voice_connection: "true", mute: mute, deaf: deaf };
-      voice_json = JSON.stringify(voice);
-      m_client.publish(topic_voice, voice_json);
-    }
   } else {
     if (newState.member.id === your_id) {
       deaf = newState.member.voice.deaf;
       mute = newState.member.voice.mute;
-      voice = { voice_connection: "true", mute: mute, deaf: deaf };
-      voice_json = JSON.stringify(voice);
-      m_client.publish(topic_voice, voice_json);
+      voice = `{"voice_connection": true, "mute": "${mute}", "deaf": "${deaf}"}`;
+      m_client.publish(topic_voice, voice);
     }
   }
 });
