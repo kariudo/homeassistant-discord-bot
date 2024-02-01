a6108150a40fcef5b89a69cc7537337e0a886924
Hunter Horsman <kariudo@gmail.com>
2024-02-01 12:58:46 -0500

    fix: export const
    Resolves (JS-E1009) Found mutable exports #8

--- src/index.ts
+++ src/index.ts
@@ -18,19 +18,12 @@ import { createMqttClient } from "./mqttClient";
 
 // Load environment variables
 dotenv.config({
-	path: "./.env",
-	encoding: "utf8",
-	defaults: "./.env.defaults",
+  path: "./.env",
+  encoding: "utf8",
+  defaults: "./.env.defaults",
 });
 
-export let config: BotConfig;
-
-try {
-	config = loadConfig();
-} catch (error) {
-	console.error("An error occurred while initializing configuration:", error);
-	process.exit(1);
-}
+export const config: BotConfig = loadConfig();
 
 // Print a fancy header banner when starting up.
 console.log("======================================");
@@ -41,9 +34,9 @@ console.log("Configuration:");
 // Tokens and passwords are masked by default for security, you can comment
 // those lines if you want to see them.
 console.debug({
-	...config,
-	bot: { ...config.bot, token: "***" },
-	mqtt: { ...config.mqtt, password: "***" },
+  ...config,
+  bot: { ...config.bot, token: "***" },
+  mqtt: { ...config.mqtt, password: "***" },
 });
 
 // Create the clients.
@@ -51,24 +44,27 @@ const discordClient: Client = createDiscordClient();
 const mqttClient: MqttClient = createMqttClient(config);
 
 // Bind handlers to listeners.
-mqttClient.on("connect", CreateHandleMqttReady(mqttClient, discordClient, config));
+mqttClient.on(
+  "connect",
+  CreateHandleMqttReady(mqttClient, discordClient, config),
+);
 mqttClient.on("error", handleMqttError);
 mqttClient.on("close", handleMqttDisconnect);
 mqttClient.on(
-	"message",
-	createHandleMqttMessage(mqttClient, discordClient, config),
+  "message",
+  createHandleMqttMessage(mqttClient, discordClient, config),
 );
 discordClient.on(
-	"ready",
-	createHandleDiscordReady(discordClient, mqttClient, config),
+  "ready",
+  createHandleDiscordReady(discordClient, mqttClient, config),
 );
 discordClient.on(
-	"voiceStateUpdate",
-	createHandleVoiceStatusUpdate(mqttClient, config),
+  "voiceStateUpdate",
+  createHandleVoiceStatusUpdate(mqttClient, config),
 );
 discordClient.on(
-	"presenceUpdate",
-	createHandlePresenceUpdate(discordClient, mqttClient, config),
+  "presenceUpdate",
+  createHandlePresenceUpdate(discordClient, mqttClient, config),
 );
 
 // Connect to Discord.
--- src/loadConfig.ts
+++ src/loadConfig.ts
@@ -9,40 +9,47 @@ import { throwEnvironmentError } from "./throwError";
  * @return {BotConfig} the loaded configuration object
  */
 export function loadConfig(): BotConfig {
-	const botTopic =
-		process.env["TOPIC_BOT"] ?? throwEnvironmentError("TOPIC_BOT");
-	return {
-		bot: {
-			token: process.env["BOT_TOKEN"] ?? throwEnvironmentError("BOT_TOKEN"),
-			nickname:
-				process.env["BOT_NICKNAME"] ?? throwEnvironmentError("BOT_NICKNAME"),
-			id: process.env["BOT_ID"] ?? throwEnvironmentError("BOT_ID"),
-		},
-		mqtt: {
-			url: process.env["MQTT_URL"] ?? throwEnvironmentError("MQTT_URL"),
-			port: process.env["MQTT_PORT"] ?? '1883',
-			username:
-				process.env["MQTT_USERNAME"] ?? throwEnvironmentError("MQTT_USERNAME"),
-			password:
-				process.env["MQTT_PASSWORD"] ?? throwEnvironmentError("MQTT_PASSWORD"),
-			clientId:
-				process.env["MQTT_CLIENT_ID"] ??
-				throwEnvironmentError("MQTT_CLIENT_ID"),
-			topics: {
-				discovery: process.env['TOPIC_DISCOVERY'] ?? 'homeassistant',
-				bot: botTopic,
-				activity: `${botTopic}/activity`,
-				connected: `${botTopic}/connected`,
-				online: `${botTopic}/online`,
-				command: `${botTopic}/command`,
-				voice: `${botTopic}/voice`,
-			},
-		},
-		guild: {
-			id: process.env["GUILD_ID"] ?? throwEnvironmentError("GUILD_ID"),
-		},
-		you: {
-			id: process.env["YOUR_ID"] ?? throwEnvironmentError("YOUR_ID"),
-		},
-	};
+  try {
+    const botTopic =
+      process.env["TOPIC_BOT"] ?? throwEnvironmentError("TOPIC_BOT");
+    return {
+      bot: {
+        token: process.env["BOT_TOKEN"] ?? throwEnvironmentError("BOT_TOKEN"),
+        nickname:
+          process.env["BOT_NICKNAME"] ?? throwEnvironmentError("BOT_NICKNAME"),
+        id: process.env["BOT_ID"] ?? throwEnvironmentError("BOT_ID"),
+      },
+      mqtt: {
+        url: process.env["MQTT_URL"] ?? throwEnvironmentError("MQTT_URL"),
+        port: process.env["MQTT_PORT"] ?? "1883",
+        username:
+          process.env["MQTT_USERNAME"] ??
+          throwEnvironmentError("MQTT_USERNAME"),
+        password:
+          process.env["MQTT_PASSWORD"] ??
+          throwEnvironmentError("MQTT_PASSWORD"),
+        clientId:
+          process.env["MQTT_CLIENT_ID"] ??
+          throwEnvironmentError("MQTT_CLIENT_ID"),
+        topics: {
+          discovery: process.env["TOPIC_DISCOVERY"] ?? "homeassistant",
+          bot: botTopic,
+          activity: `${botTopic}/activity`,
+          connected: `${botTopic}/connected`,
+          online: `${botTopic}/online`,
+          command: `${botTopic}/command`,
+          voice: `${botTopic}/voice`,
+        },
+      },
+      guild: {
+        id: process.env["GUILD_ID"] ?? throwEnvironmentError("GUILD_ID"),
+      },
+      you: {
+        id: process.env["YOUR_ID"] ?? throwEnvironmentError("YOUR_ID"),
+      },
+    };
+  } catch (error) {
+    console.error("An error occurred while initializing configuration:", error);
+    process.exit(1);
+  }
 }
